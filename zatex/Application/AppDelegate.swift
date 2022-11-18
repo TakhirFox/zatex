58aa302a60982d73e0b46d91da8a7da0c02a8cd7
TakhirFox <iamtheorangefox@gmail.com>
2022-11-19 01:48:51 +0700

    Connect FLEX to appDelegate

--- zatex/Application/AppDelegate.swift
+++ zatex/Application/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import FLEX
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         Appearance.apply()
-        
+        FLEXManager.shared.showExplorer()
         return true
     }
 
