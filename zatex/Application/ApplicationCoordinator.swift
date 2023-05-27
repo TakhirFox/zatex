99cc23161f82dc233fdeecd37401b158bc315af9
winzero <iamtheorangefox@gmail.com>
2023-05-28 01:59:39 +0700

    ZATEX-18 Correct auth is done

---
+++ zatex/Application/ApplicationCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  ApplicationCoordinator.swift
+//  zatex
+//
+//  Created by Zakirov Tahir on 27.05.2023.
+//
+
+import Foundation
---
+++ zatex/Base/SessionProvider/SessionProvider.swift
@@ -0,0 +1,19 @@
+//
+//  SessionManager.swift
+//  zatex
+//
+//  Created by Zakirov Tahir on 25.05.2023.
+//
+
+import Foundation
+
+class SessionManager {
+    var isAuthorized: Bool {
+        
+    }
+    
+    func getSession() -> String? {
+        
+    }
+
+}
